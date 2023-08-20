68888f81241b90a8fd7cab88aeaffdc15fabba6c
Luis <134321059+LuisAngelRI@users.noreply.github.com>
2023-08-20 17:48:16 +0000

    Agregar Integrante

--- src/main/js/pages/ver-banda.js
+++ src/main/js/pages/ver-banda.js
@@ -1,18 +1,14 @@
 const React = require('react');
 const client = require('../client');
-const { Link, useParams} = require('react-router-dom');
+const { Link, useParams, } = require('react-router-dom');
 const {useState, useEffect} = require('react');
 
 
-
-const PageVerBanda = (props) => {
+const PageVerBanda = () => {
 
     let { id } = useParams();
     const [banda, setBanda] = useState({});
-    // const [integrantes, setIntegrantes] = useState([]);
     const [integrantes, setIntegrantes] = useState([]);
-
-
     useEffect(() => {
         url_banda = '/api/bandas/' + id
         client({
@@ -23,14 +19,14 @@ const PageVerBanda = (props) => {
             method: 'GET',
             path: url_banda + '/formacion'
         }).done(response => setIntegrantes(response.entity))
-       
+        
     }, []);
 
 
     return (
         <>
             <h1>Banda</h1>
-            <table>
+            <table border="1">
                 <tbody>
                     <tr>
                         <th>Nombre</th>
@@ -42,11 +38,33 @@ const PageVerBanda = (props) => {
             <hr />
 
             <h2>integrantes</h2>
-            <table>
 
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Musico</th>
+                        <th>Instrumento</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {integrantes.map(integrante => {
+
+                        return (
+                            <tr key={integrante.ID}>
+                                <td>{integrante.MUSICO}</td>
+                                <td>{integrante.INSTRUMENTO}</td>
+                            </tr>
+                        )
+
+                    })}
+
+                </tbody>
             </table>
-
+            <hr />
+            <Link to={`/ver-banda/${id}/nuevo-integrante`}>Agregar integrante</Link> |  
             <Link to="/">Volver</Link>
+            
         </>
     )
 }
